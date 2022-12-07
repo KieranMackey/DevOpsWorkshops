917b5865a709e406d48db2d65977e0342b1bf950
KieranMackey <69765305+KieranMackey@users.noreply.github.com>
2022-12-07 14:18:37 +0000

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,35 @@
+
+import sys
+
+def basic_calc(op, val1, val2):
+    # Check inputs are ints
+
+    # Perform operation
+    if(op == '/'):
+        retval = val1 / val2
+    elif(op == 'x'):
+        retval = val1 * val2
+    elif(op == '+'):
+        retval = val1 + val2
+    elif(op == '-'):
+        retval = val1 + val2
+    else:
+        retval = ("Error: no valid operator supplied")
+    return retval
+
+def main(argv):
+    # Test cases
+    print(basic_calc('+',1,2))
+    print(basic_calc('/',5,2))
+    print(basic_calc('x',5,0))
+
+    with open("step_2.txt", 'r') as f:
+        lines = f.readlines()
+
+    line = 1
+    print (str(len(lines)))
+    #while (line < len(lines))
+
+
+if __name__ == "__main__":
+   main(sys.argv)
